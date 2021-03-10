fbde0ca384b8ca45db66f0d5f6103d2311f49ec0
Allie Saari <alliesaari@Allies-MacBook-Air-2.local>
2021-03-09 21:26:14 -0500

    input text

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,8 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Gator Communications';
+function translate() {
+    var x = document.getElementById("Input").value;
+
+    document.getElementById("demo").innerHTML = x;
+}
+
